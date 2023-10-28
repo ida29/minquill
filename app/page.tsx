388d34ca592c38d31b98c067a3c1eb710ab52b77
ida29 <y.ida898@gmail.com>
2023-10-28 22:07:33 +0900

    Add Google Auth with NextAuth

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+'use client';
+import { useSession } from 'next-auth/react';
 import { css } from "@/styled-system/css";
+import { LoginButton, LogoutButton } from "@/app/components/buttons";
 
 export default function Home() {
-  return (
-    <main>
+    const { data: session, status } = useSession();
+    return (
+        <main>
+		    {status == 'authenticated' ? (<p>ログイン</p>) : (<p>ログアウト</p>)}
+            <div>
+                <LoginButton/>
+                <LogoutButton/>
+            </div>
       <h1 className={css({ fontSize: "2xl", fontWeight: "bold" })}>
         Hello 🐼!
       </h1>
-    </main>
-  )
+        </main>
+    );
 }
