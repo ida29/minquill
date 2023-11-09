76ef3cd641658bc961e7a8d03216beffdfbb0866
ida29 <y.ida898@gmail.com>
2023-11-10 06:44:40 +0900

    Update home_page_body.tsx

--- components/home_page_body.tsx
+++ components/home_page_body.tsx
@@ -1,6 +1,7 @@
 // components/home_page_body.tsx
 "use client";
 
+import Link from "next/link";
 import { css, cva } from "@/styled-system/css";
 import { Dictionary } from "@/app/[lang]/dictionary";
 import { getPostsByUsername, Post } from "@/app/[lang]/post";
@@ -29,7 +30,9 @@ export const HomePageBody = (params: { dict: Dictionary }) => {
           >
             {posts.map((post, index) => (
               <div key={index} className={contentStyle()}>
-                <div>{post.title}</div>
+                <Link href={`/${post.authorId}/posts/${post.ulid}`}>
+                  {post.title}
+                </Link>
               </div>
             ))}
             <div>{params.dict.end}</div>
