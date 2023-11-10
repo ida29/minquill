393f8b3dbb803cbb254a4fae3aaf37adf3901f00
ida29 <y.ida898@gmail.com>
2023-11-11 04:13:38 +0900

    Update auth page

--- app/[lang]/auth/signin/page.tsx
+++ app/[lang]/auth/signin/page.tsx
@@ -1,7 +1,8 @@
 // app/[lang]/auth/signin
-import { cva } from "@/styled-system/css";
+import { cva, css } from "@/styled-system/css";
 import { SigninWithGoogleBtn } from "@/components/sign_in_with_google_btn";
 import { getDictionary } from "@/app/[lang]/dictionary";
+import { CloudflareImage } from "@/components/cloudflare_image";
 
 export default async function App({
   params: { lang },
@@ -12,7 +13,32 @@ export default async function App({
 
   return (
     <div className={mainStyle()}>
-      <SigninWithGoogleBtn text={dict.sign_in_with_google} />
+      <div
+        className={css({
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+          flexDirection: "column",
+        })}
+      >
+        <CloudflareImage
+          src="a717cf59-f0ef-409a-d96b-904475a64c00"
+          alt="LowPoly Mage Image"
+          className={css({
+            width: "50%",
+            //marginLeft: "auto",
+            alignSelf: "center",
+            marginTop: "1.6rem",
+            sm: { width: "40%" },
+            md: { width: "30%" },
+            lg: { width: "20%" },
+            padding: "0 1rem 1rem 0",
+          })}
+          variants="public"
+        />
+        <SigninWithGoogleBtn text={dict.sign_in_with_google} />
+      </div>
     </div>
   );
 }
